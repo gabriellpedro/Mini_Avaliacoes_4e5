586ebefb117e51667c265552435b6e38c727054e
gabriellpedro <gabriel.pedro47@hotmail.com>
2023-05-06 23:40:41 -0300

    Adiciona testes

--- todolist/core/tests.py
+++ todolist/core/tests.py
@@ -2,8 +2,20 @@ from django.test import TestCase
 from core.forms import TaskForm
 from core.models import TaskModel
 
-class TaskModelTest(TestCase):
+class Home_Test(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
 
+    def test_200_response(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_text(self):
+        self.assertContains(self.response, 'To-Do List:')
+
+    def test_template_used(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+
+class TaskModelTest(TestCase):
     def setUp(self):
         self.nome_atividade = 'Feriado do dia 3/05'
         self.data_atividade = "2023-05-03"
@@ -16,10 +28,17 @@ class TaskModelTest(TestCase):
     def test_created(self):
         self.assertTrue(TaskModel.objects.exists())
 
-
     def test_str_do_objeto(self):
         objeto = TaskModel.objects.all()[0]
         self.assertEqual(str(objeto), self.nome_atividade)
+
+    def test_nome_atividade(self):
+        nome = self.atividade.__dict__.get('nome_atividade', '')
+        self.assertEqual(nome, self.nome_atividade)
+
+    def test_data_atividade(self):
+        data = self.atividade.__dict__.get('data_atividade', '')
+        self.assertEqual(data, self.data_atividade)
 
 class TaskFormTest(TestCase):
     def test_form_has_fields(self):
@@ -27,6 +46,12 @@ class TaskFormTest(TestCase):
         expected =  ['nome_atividade','data_atividade']
         self.assertSequenceEqual(expected, list(form.fields))
 
-class coreGetCadastro(TestCase):
-    def setUp(self):
-        self.resp = self.client.get(r('core:cadastro'))
+    def make_validated_form(self, **kwargs):
+        valid = dict(
+            nome_atividade = 'Dia da mentira',
+            data_atividade = "01/04/2023"
+        )
+        data = dict(valid, **kwargs)
+        form = TaskForm(data)
+        form.is_valid()
+        return form
